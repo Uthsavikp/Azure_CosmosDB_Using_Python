4e8ebc62555d6357f9ecee656b8916bdda14c3ee
Uthsavi KP <uthsavi29@gmail.com>
2021-02-24 09:41:16 +0530

    Modified:Wrong commit message -added stored procedure file written in javascript for sql api

--- Stored_Procedure/spCreateToDoItems.js
+++ Stored_Procedure/spCreateToDoItems.js
@@ -1,4 +1,11 @@
 // STORED PROCEDURE
+
+//@Author: Uthsavi KP
+//@Date: 2020-02-18 21:11:56
+// @Last Modified by: Uthsavi KP
+//@Last Modified time: 2020-02-18 21:11:56
+//@Title : To Write Stored Procedure Written In Javascript For SQL Api And Run It Using Python
+
 function sample(prefix) {
     var collection = getContext().getCollection();
 
